01a7fcc223a6c833f37725ec6f91ba384674a15c
SKZ <skz@apple.com>
2024-03-26 13:06:00 +1100

    main page

--- src/components/Home/Main.tsx
+++ src/components/Home/Main.tsx
@@ -7,7 +7,7 @@ export default function Main() {
         <p className="text-accent-color-0 py-1">Hi, my name is</p>
         <h1 className="font-bold text-4xl sm:text-7xl text-white">Sanjeet Kanwar</h1>
         <h2 className="font-bold text-4xl sm:text-7xl text-gray-400">I'm a Frontend Developer</h2>
-        <p className="text-gray-400 py-4">Highly driven and performance focused with over 10 years of hands-on experience designing and developing responsive web applications.</p>
+        <p className="text-gray-400 py-4">Highly driven and performance focused Software Engineer with over 10 years of hands-on experience designing and developing responsive web applications.</p>
       </div>
 
       <div className="absolute bottom-3 animate-bounce text-white">
